87d9a396ee8afa71240d4802c1d5f6e3e040bcfe
DevZunxBR <devzunxbr@gmail.com>
2026-04-15 19:44:16 -0300

    Config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'  // ← MUDE AQUI (remove o @/)
+import './index.css'          // ← MUDE AQUI TAMBÉM
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
