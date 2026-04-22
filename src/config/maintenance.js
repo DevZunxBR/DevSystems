10eb6f5900cfb892a5e7f4dea9b70257b1e33ade
DevZunxBR <devzunxbr@gmail.com>
2026-04-21 23:11:36 -0300

    FormularioPagina

--- src/config/maintenance.js
+++ src/config/maintenance.js
@@ -1,7 +1,7 @@
 // src/config/maintenance.js
 export const maintenanceConfig = {
   // Ativar/desativar manutenção
-  enabled: false, // mude para false quando quiser liberar
+  enabled: true, // mude para false quando quiser liberar
   
   // Emails que podem acessar mesmo em manutenção
   whitelistEmails: [
