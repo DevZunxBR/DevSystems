0addd665b39d88784fb638f43507a9418068d546
DevZunxBR <devzunxbr@gmail.com>
2026-05-02 20:07:16 -0300

    AsaasAPITest

--- src/config/asaas.js
+++
@@ -1,6 +0,0 @@
-// src/config/asaas.js
-export const asaasConfig = {
-  // Usar sandbox para testes, depois trocar para produção
-  baseURL: 'https://sandbox.asaas.com/api/v3', // Produção: 'https://www.asaas.com/api/v3'
-  apiKey: '$aact_hmlg_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6Ojk3NzZhYWZjLTI4MDctNDUwYy05NjU4LTAzMGYwMTAyYmY3NDo6JGFhY2hfZTc4NjEwNDYtMzFlMS00ZTlhLTk0ZDQtODYzOWI3YWEyZTk5', // Cole sua API Key aqui
-};
